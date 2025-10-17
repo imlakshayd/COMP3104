7caff6ae4a60cf3208b7e4187241d3e100aa4327
Lakshay <lakshayd2002@gmail.com>
2025-10-16 20:48:54 -0400

    Changed homepage

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101464867 || Lakshay Dhawan || DevOps</h5>
+      <h5>Developed By: 101464867 || Lakshay Dhawan || COMP 3104 || DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
